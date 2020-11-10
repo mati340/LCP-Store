3bb2fd0bc31a6d890c61f13f56c6ce0472968fc0
Matan Katz <mati340@gmail.com>
2020-11-10 17:56:02 +0200

    Added Remove from cart button with ajax

--- LCPStore/Controllers/CartItemsController.cs
+++ LCPStore/Controllers/CartItemsController.cs
@@ -25,8 +25,19 @@ namespace LCPStore.Controllers
             return View(await _context.CartItem.ToListAsync());
         }
 
+        // POST: CartItems/Delete/5
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public async Task<double> Delete(int id)
+        {
+            var cartItem = await _context.CartItem.Include(c => c.Cart).FirstOrDefaultAsync(c => c.Id == id);
+            cartItem.Cart.SumToPay -= cartItem.TotalPrice;
+            _context.CartItem.Remove(cartItem);
+            await _context.SaveChangesAsync();
+            return cartItem.Cart.SumToPay;
+        }
 
-        private bool CartItemExists(int id)
+            private bool CartItemExists(int id)
         {
             return _context.CartItem.Any(e => e.Id == id);
         }
