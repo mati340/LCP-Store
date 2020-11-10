114fbb7f9ec8344635f9d714c8c9c2c396ee22d7
Matan Katz <mati340@gmail.com>
2020-11-10 20:06:16 +0200

    Fixed add to cart with AJAX

--- LCPStore/wwwroot/js/Cart/AddToCart.js
+++ LCPStore/wwwroot/js/Cart/AddToCart.js
@@ -1,25 +1,47 @@
-﻿$(function () {
+﻿
 
+//$(function () {
 
-    var p = $('#productId').val();
-    var q = $('#quantity').val(); 
+//    $('#AddToCart').submit(function () {
+//    var p = $('#productId').val();
+//    var q = $('#quantity').val(); 
 
-
-    $('#AddToCart').submit(function () {
-
-            // שליחת בקשת Ajax
-            $.ajax({
-                url: "/Carts/AddToCart", // מיקום הקובץ אליו תשלח הבקשה
-                data: {
-                    productId: p,
-                    quantity: q
-                }, // איזה מידע לשלוח לקובץ
-                success: function (callback) { // ברגע שהבקשה נשלחה והצליחה , קבלה של נתונים חזרה - callback
-                    console.log(callback);
-                }
-            });
-            return false;
-        }
+//            // שליחת בקשת Ajax
+//            $.ajax({
+//                url: "/Carts/AddToCart", // מיקום הקובץ אליו תשלח הבקשה
+//                type: "POST",
+//                data: {
+//                    productId: p,
+//                    quantity: q
+//                }, // איזה מידע לשלוח לקובץ
+//                success: function (callback) { // ברגע שהבקשה נשלחה והצליחה , קבלה של נתונים חזרה - callback
+//                    console.log(callback);
+//                }
+//            });
+//            return false;
+//        }
         
-    );
-})
+//    );
+//})
+
+
+$('a.add-card').on("click", function (event) {
+    event.preventDefault()
+    var p = parseInt($(this).closest('div').prop('id'));
+
+    // שליחת בקשת Ajax
+    $.ajax({
+        url: "/Carts/AddToCart", // מיקום הקובץ אליו תשלח הבקשה
+        type: "POST",
+        data: {
+            productId: p,
+        }, // איזה מידע לשלוח לקובץ
+        success: function (callback) { // ברגע שהבקשה נשלחה והצליחה , קבלה של נתונים חזרה - callback
+            alert("The product added to cart successfully");
+        },
+        error: function (callback) {
+            alert("Can't add this product to cart");
+        },
+    });
+
+});
