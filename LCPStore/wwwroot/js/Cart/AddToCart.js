56fcd04118a901c1bf01ea352ac8e3d950a73a7f
Matan Katz <mati340@gmail.com>
2020-11-11 23:18:35 +0200

    fixed category search

--- LCPStore/wwwroot/js/Cart/AddToCart.js
+++ LCPStore/wwwroot/js/Cart/AddToCart.js
@@ -1,30 +1,4 @@
 ﻿
-
-//$(function () {
-
-//    $('#AddToCart').submit(function () {
-//    var p = $('#productId').val();
-//    var q = $('#quantity').val(); 
-
-//            // שליחת בקשת Ajax
-//            $.ajax({
-//                url: "/Carts/AddToCart", // מיקום הקובץ אליו תשלח הבקשה
-//                type: "POST",
-//                data: {
-//                    productId: p,
-//                    quantity: q
-//                }, // איזה מידע לשלוח לקובץ
-//                success: function (callback) { // ברגע שהבקשה נשלחה והצליחה , קבלה של נתונים חזרה - callback
-//                    console.log(callback);
-//                }
-//            });
-//            return false;
-//        }
-        
-//    );
-//})
-
-
 $('form#AddCartForm').submit(function (event) {
     event.preventDefault()
     var p = $('#productId').val();
