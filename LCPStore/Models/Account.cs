1e1f612bc8eb9f890d85a45231165a6b1458ff78
Lee Lisker <Lee Lisker@DESKTOP-SQAJ41L>
2020-11-12 00:17:14 +0200

    updates

--- LCPStore/Models/Account.cs
+++ LCPStore/Models/Account.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -19,12 +20,20 @@ namespace LCPStore.Models
     public class Account
     {
         public int Id { get; set; }
+        [Required]
+        [DisplayName("Ëmail")]
+        [EmailAddress]
         public string Username { get; set; }
         [DataType(DataType.Password)]
+        [Required]
         public string Password { get; set; }
+        [Required]
         public Gender Gender { get; set; }
+        [Required]
         public string Name { get; set; }
         [DataType(DataType.Date)]
+        [Required]
+        [DisplayName("Birth Date")]
         public DateTime BirthDate { get; set; }
         [DataType(DataType.DateTime)]
         public DateTime Registered { get; set; }
